968c00d11abb6e3272a719c04c823940e0b58368
Barry Ali <pavelnemesis288@gmail.com>
2026-02-05 14:30:00 +0000

    feat:ali: Ajout DAO Produit

--- src/main/model/dao/ProduitDAO.java
+++ src/main/model/dao/ProduitDAO.java
@@ -4,10 +4,21 @@
  */
 package main.model.dao;
 
+import main.model.entite.Produit;
+
 /**
  *
  * @author isaac
  */
 public class ProduitDAO {
     
+    // DAO pour inserer un produit
+    public void insert(Produit produit){
+        String sql = "INSERT INTO Produit (nom, idCategorie, prixDeVente, stockActuel, seuilAlerte) VALUES(?, ?, ?, ?, ?)";
+        
+        try(Connection conn = DBConnection.getConnection();
+                PreparedStatement pst = conn.prepareStatement(sql);
+        } catch (){
+
+        }
 }

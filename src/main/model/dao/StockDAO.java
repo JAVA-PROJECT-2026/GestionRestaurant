3c56b1b2e6216e4bfa667feeacb278a0c4a12691
Barry Ali <pavelnemesis288@gmail.com>
2026-02-06 16:43:25 +0000

    feat/ali: Ajout de DOA pour Stock mais reste encore

--- src/main/model/dao/StockDAO.java
+++ src/main/model/dao/StockDAO.java
@@ -4,10 +4,22 @@
  */
 package main.model.dao;
 
+import main.model.entite.Produit;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author isaac
  */
 public class StockDAO {
-    
+    public void insertStock(Stock stock) {
+        String sql = "INSERT INTO Stock (typeMouv, idProd, quantite, dateMouv, motif");
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pst = PrepareStaement(sql);) {
+            
+            pst.setString(1, st);
+        }
+    }
 }

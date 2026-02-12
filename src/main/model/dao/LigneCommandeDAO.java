2089e6754542053d6bf1b688dcc29742cd9f097c
sabour-agoro <abdoulsabouragoro@gmail.com>
2026-02-12 10:16:12 +0100

    construction partielle de ligne commande

--- src/main/model/dao/LigneCommandeDAO.java
+++ src/main/model/dao/LigneCommandeDAO.java
@@ -3,13 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main.model.dao;
+import java.sql.*;
 
 /**
  *
  * @author isaac
  */
-public class LigneCommande {
+import main.model.entite.LigneCommande;
+import main.util.DatabaseConnection;
+public class LigneCommandeDAO {
+    public void insert(LigneCommande lignecommande) {
+        String sql = "INSERT INTO LigneCommande(idCom, idProd, quantite, prixUnitaire, montantLigne) VALUES(?, ?, ?, ?, ?)";
+        
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+        
+            
+        
+        }catch(SQLException e){
+            
+        }
+        
     
+    
+    
+    
+    }
     
     
 }

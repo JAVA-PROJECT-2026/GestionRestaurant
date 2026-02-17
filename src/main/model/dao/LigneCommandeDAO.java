dc733c2144bf1a5fb24a4dc1456541c19ea577ef
Barry Ali <pavelnemesis288@gmail.com>
2026-02-17 00:42:37 +0000

    feat/ali: Résolution d'imports

--- src/main/model/dao/LigneCommandeDAO.java
+++ src/main/model/dao/LigneCommandeDAO.java
@@ -1,6 +1,4 @@
 
-package main.model.dao;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/model/dao/StockDAO.java
+++ src/main/model/dao/StockDAO.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package main.model.dao;
-
 import main.util.DatabaseConnection;
 import main.model.entite.MouvementStock;
 import main.model.entite.enums.TypeMouvement;

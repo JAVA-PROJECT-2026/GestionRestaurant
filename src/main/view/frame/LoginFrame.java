22ca29c29123a685406ed0073b306da6ac73bc90
Isaac DJIMADJO <isaacdjimadjo830@gmail.com>
2026-02-17 15:43:59 +0000

    login frame

--- src/main/view/frame/LoginFrame.java
+++ src/main/view/frame/LoginFrame.java
@@ -290,6 +290,7 @@ public class LoginFrame extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new LoginFrame().setVisible(true);
             }
